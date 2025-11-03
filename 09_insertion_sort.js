196f939e48f935df2194b22bfe77ef5ac061fa59
sreevatsava.garisa <sreevatsava.garisa@assettl.com>
2025-11-03 11:32:09 +0530

    Updated Insertion Sort

--- 09_insertion_sort.js
+++ 09_insertion_sort.js
@@ -3,6 +3,33 @@
 Time: O(n^2)
 Space: O(1)
 */
+function solution_1(arr) {
+    if (arr.length <= 1) return arr;
+
+    for (let i = 1; i < arr.length; i++) {
+
+        let key = arr[i];
+        let j = i - 1;
+
+        while (j >= 0) {
+            if (arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            } else {
+                break;
+            }
+        }
+
+        arr[j + 1] = key;
+    }
+
+    return arr;
+}
+/*****************************************************************************************************************************/
+/*
+Time: O(n^2)
+Space: O(1)
+*/
 function solution_2(arr) {
     if (!arr.length) return [];
     if (arr.length == 1) return arr;
