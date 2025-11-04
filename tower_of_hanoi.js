46a7c5dedca613ce2e48f848c2c222de8fc014bb
sreevatsava.garisa <sreevatsava.garisa@assettl.com>
2025-11-04 11:38:39 +0530

    Tower of Hanoi

--- tower_of_hanoi.js
+++ tower_of_hanoi.js
@@ -1,9 +1,12 @@
 
 /*
-Time: O()
-Space: O()
+Time: O(2^n)
+Space: O(1)
 */
-function solution_1() {
-
+const towerOfHanoi = (n, origin, destination, temporary) => {
+    if (n === 0) return;
+    towerOfHanoi(n - 1, origin, temporary, destination);
+    console.log(`Disc number ${n} is moved from pile${origin} to pile${destination}.`)
+    towerOfHanoi(n - 1, temporary, destination, origin);
 }
-
+towerOfHanoi(3, 'A', 'C', 'B');
