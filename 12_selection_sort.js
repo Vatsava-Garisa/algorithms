00f857ea2b0027fc759283e889464c4b52461b1b
sreevatsava.garisa <sreevatsava.garisa@assettl.com>
2025-11-04 15:06:25 +0530

    Updated Selection Sort

--- 12_selection_sort.js
+++ 12_selection_sort.js
@@ -22,3 +22,35 @@ function solution_1(arr) {
 
     return arr;
 }
+/*****************************************************************************************************************************/
+/*
+Time: O(n^2)
+Space: O(1)
+*/
+function stable_selection_sort(arr) {
+    if (arr.length <= 1) return arr;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        // Find the minimum element in the unsorted portion
+        let min_index = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+        }
+
+        // Extract the min value
+        let min_value = arr[min_index];
+
+        // Shift elements right to make space
+        while (min_index > i) {
+            arr[min_index] = arr[min_index - 1];
+            min_index--;
+        }
+
+        // Place min_value at correct sorted position
+        arr[i] = min_value;
+    }
+
+    return arr;
+}
